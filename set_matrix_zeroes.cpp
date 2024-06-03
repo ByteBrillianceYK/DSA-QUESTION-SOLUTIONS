2aa2a689408fc826549bcc3e4b332e36b20cad0e
Yadnyesh Khotre <yadnyeshkhotre.dev@gmail.com>
2024-06-03 07:13:22 +0530

    Update set_matrix_zeroes.cpp

--- set_matrix_zeroes.cpp
+++ set_matrix_zeroes.cpp
@@ -40,3 +40,34 @@ public:
         }
     }
 };
+
+
+
+//2. Slightly Better Way using less time complexity:
+class Solution {
+public:
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        vector<int> row(rows,0);
+        vector<int> col(cols,0);
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(row[i] || col[j]){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+};
