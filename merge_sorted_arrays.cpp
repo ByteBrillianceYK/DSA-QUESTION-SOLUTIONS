f814ae548d1f76c6712a5a496bc0c88241bba2a5
Yadnyesh Khotre <yadnyeshkhotre.dev@gmail.com>
2024-06-10 20:06:48 +0530

    Update merge_sorted_arrays.cpp

--- merge_sorted_arrays.cpp
+++ merge_sorted_arrays.cpp
@@ -1,4 +1,6 @@
+//Question Link:https: //leetcode.com/problems/merge-sorted-array/
 // 1.Brute Force Approach
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -26,5 +28,27 @@ public:
         nums1=nums3;
 
         
+    }
+};
+
+// 2. Optimal Approach:
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int left=m-1,right=0;
+        while(left>=0 && right<n){
+            if(nums1[left]>nums2[right]){
+                swap(nums1[left],nums2[right]);
+                left--,right++;
+            }else{
+                break;
+            }
+        }
+      
+        for(int i=0;i<n;i++){
+            nums1[m+i]=nums2[i];
+        }
+
+        sort(nums1.begin(), nums1.end());
     }
 };
