806d11c2fa924d2facc25a3ea26925f234299abf
Yadnyesh Khotre <yadnyeshkhotre.dev@gmail.com>
2024-06-13 19:11:16 +0530

    Update repeat_missing_number_array.cpp

--- repeat_missing_number_array.cpp
+++ repeat_missing_number_array.cpp
@@ -2,24 +2,27 @@
 // 1. Brute Force Approach:
 vector<int> Solution::repeatedNumber(const vector<int> &A) {
     vector<int> sorted_A=A;
+    sort(sorted_A.begin(),sorted_A.end());
     int size=sorted_A.size();
     int repeated=-1;
     int missing=-1;
-    sort(sorted_A.begin(),sorted_A.end());
+    
     for(int i=0;i<size-1;i++){
         if(sorted_A[i]==sorted_A[i+1]){
-            repeated=(sorted_A[i]);
+            repeated=sorted_A[i];
         }
+        
         if(sorted_A[i]+1<sorted_A[i+1]){
             missing=(sorted_A[i]+1);
         }
     }
-    if(missing==-1){
-        if(sorted_A[0]!=1){
-            missing=1;
-        }else if(sorted_A[size-1]!=size){
+    
+      if(sorted_A[0]!=1){
+        missing=1;
+    }else if(sorted_A.back()!=size){
         missing=size;
-    }
-    }
+    }      
+    
     return {repeated,missing};
 }
+
