0df2e16f9f09f83edcc9e48f9b2bc4831a2cc543
Yadnyesh Khotre <yadnyeshkhotre.dev@gmail.com>
2024-06-13 20:06:57 +0530

    Update repeat_missing_number_array.cpp

--- repeat_missing_number_array.cpp
+++ repeat_missing_number_array.cpp
@@ -26,3 +26,28 @@ vector<int> Solution::repeatedNumber(const vector<int> &A) {
     return {repeated,missing};
 }
 
+//2. Optimized Approach using simple Maths:
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    long long n=A.size();
+    long long sum=0, n_sum, sqr_sum=0, sqr_sum_n;
+    
+    for(int i=0;i<n;i++){
+        sum+=A[i];
+        sqr_sum+=(long long)A[i]* (long long)A[i];
+    }
+    
+    n_sum=(n*(n+1))/2;
+    sqr_sum_n=(n*(n+1)*((2*n)+1))/6;
+    
+    long long r, m;
+    long long r_minus_m=sum-n_sum;
+    long long r_sqr_minus_m_sqr=sqr_sum-sqr_sum_n;
+    
+    long long r_plus_m = r_sqr_minus_m_sqr / r_minus_m;
+    long long repeat=(r_minus_m+r_plus_m)/2;
+    long long missing=repeat-(r_minus_m);
+        
+    
+    return {(int)repeat,(int)missing};
+}
+
