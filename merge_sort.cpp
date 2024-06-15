343de7ee1a0bc24bee0aaeda6e1791d8c3c8d6ab
Yadnyesh Khotre <yadnyeshkhotre.dev@gmail.com>
2024-06-15 11:54:58 +0530

    Create merge_sort.cpp

---
+++ merge_sort.cpp
@@ -0,0 +1,39 @@
+void merge(vector < int > & arr, int low, int mid, int high){
+    vector < int > temp;
+    int left=low, right=mid+1;
+    while((left<=mid) && (right<=high)){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        } else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i=low;i<=high;i++){
+        arr[i]=temp[i-low];
+    }
+}
+
+void mSort(vector < int > & arr, int low, int high){
+    if(low==high) return;
+    int mid=(low+high)/2;
+    mSort(arr, low, mid);
+    mSort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+}
+
+void mergeSort(vector < int > & arr, int n) {
+    // Write your code here.
+    mSort(arr, 0, n-1);
+    
+}
