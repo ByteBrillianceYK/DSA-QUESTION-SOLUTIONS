b10ce5ff51c271b2d5e29f3b96f0503bdb8669dd
Yadnyesh Khotre <yadnyeshkhotre.dev@gmail.com>
2024-06-16 19:59:08 +0530

    Update count_inversions.cpp

--- count_inversions.cpp
+++ count_inversions.cpp
@@ -16,3 +16,50 @@ long long getInversions(long long *arr, int n){
 
     return pair;
 }
+
+//2. Optimized Approach using merge sort:
+#include <bits/stdc++.h>
+
+long long pairs=0;
+void merge(long long *arr, int low, int mid, int high){
+    vector < long long > temp;
+    int left=low, right=mid+1;
+    while((left<=mid) && (right<=high)){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        } else {
+            temp.push_back(arr[right]);
+            pairs+=(mid-left+1);
+            right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i=low;i<=high;i++){
+        arr[i]=temp[i-low];
+    }
+}
+
+void mSort(long long *arr, int low, int high){
+    if(low==high) return;
+    int mid=(low+high)/2;
+    mSort(arr, low, mid);
+    mSort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+}
+
+
+
+long long getInversions(long long *arr, int n){
+    // Write your code here
+    mSort(arr, 0, n-1);
+    return pairs;
+}
